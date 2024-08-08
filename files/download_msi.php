b3feb7e91296f625201dcaba64c8eb16bdc8bfaa
Wojciech Stark <66268167+wgst@users.noreply.github.com>
2024-08-08 15:44:01 +0200

    Upload files

---
+++ files/download_msi.php
@@ -0,0 +1,15 @@
+<?php
+
+$var = @file_get_contents('count_msi.txt');
+$date = date('Y-m-d H:i:s') . ';' . $_SERVER['REMOTE_ADDR']; 
+$file="count_msi.txt";
+$linecount = 0;
+$lines_arr = preg_split('/\n|\r/',$var);
+$linecount = count($lines_arr); 
+
+$linecount++;
+$var = $var ."\n". $linecount . ';' . $date;
+
+@file_put_contents('count_msi.txt', $var);
+header('Location: http://www.do-exp.com/files/Do-Exp1.0.0.0.msi');
+?>
---
+++ files/download_rar.php
@@ -0,0 +1,18 @@
+<?php
+
+$var = @file_get_contents('count_rar.txt');
+$date = date('Y-m-d H:i:s') . ';' . $_SERVER['REMOTE_ADDR']; 
+$file="count_rar.txt";
+$linecount = 0;
+$handle = fopen($file, "r");
+
+while(!feof($handle)){
+  $line = fgets($handle);
+  $linecount++;
+}
+$linecount++;
+$var = $var ."\n". $linecount . ';' . $date;
+
+@file_put_contents('count_rar.txt', $var);
+header('Location: http://www.do-exp.com/files/Do-Exp1.0.0.0.rar');
+?>
